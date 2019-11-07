a21296d344cb8c9b1e2d6b95f2f11bef16eb7077
Ilya Dovgopol <idovgopol@e2r9p6.unit.ua>
2019-11-07 17:02:16 +0200

    refreshmarathonc

--- t04/mx_printint.c
+++ t04/mx_printint.c
@@ -5,16 +5,16 @@ void mx_printchar(char c);
 void mx_printint(int n) {
 	if (n == -2147483648) {
 		write(1, "-2147483648", 11);
-		return;
 	}
-	if (n < 0) {
+	else if (n < 0) {
 		mx_printchar('-');
-		n = -n;
+		mx_printint(n * -1);
 	}
-	if (n > 9) {
+	else if (n < 10) {
+		mx_printchar(n + '0');
+	}
+	else {
 		mx_printint(n / 10);
-	}
-	if (n > 9 || n < 10) {
 		mx_printchar(n % 10 + '0');
 	}
 }

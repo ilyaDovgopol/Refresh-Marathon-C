a815bc885f78eb3f04972cc37df16872f3862f75
Ilya Dovgopol <idovgopol@e2r14p4.unit.ua>
2019-12-12 12:54:44 +0200

    refreshmarathonc

--- t16/mx_strnew.c
+++ t16/mx_strnew.c
@@ -4,10 +4,7 @@
 char *mx_strnew(const int size) {
     char *s = (char *)malloc((size + 1) * sizeof(char));
 
-    if (size < 0) {
-        return NULL;
-    }
-    if (!s) {
+    if (size < 0 || !s) {
         return NULL;
     }
     for (int i = 0; i < size + 1; i++) {

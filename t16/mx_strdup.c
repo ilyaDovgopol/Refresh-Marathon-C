42ff89aaa88792a5966abedc9449e8cae643fc24
Ilya Dovgopol <idovgopol@e2r14p4.unit.ua>
2019-12-12 13:45:11 +0200

    refreshmarathonc

--- t16/mx_strdup.c
+++ t16/mx_strdup.c
@@ -1,12 +1,17 @@
 #include <stddef.h>
 
-char *mx_strcpy(char *dst, const char *src);
 char *mx_strnew(const int size);
 int mx_strlen(const char *s);
+char *mx_strcpy(char *dst, const char *src);
 
 char *mx_strdup(const char *s1) {
-    char *s = mx_strcpy(mx_strnew(mx_strlen(s1)), s1);
+    char *s = mx_strnew(mx_strlen(s1));
+    char *str;
 
-    return s;
+    if (!s) {
+    	return NULL;
+    }
+    str = mx_strcpy(s, s1);
+    return str;
 }
 

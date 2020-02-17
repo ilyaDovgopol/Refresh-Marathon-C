7d15ca65cff2e44b71bac1502b2f75f11d2fb103
Ilya Dovgopol <idovgopol@e2r13p1.unit.ua>
2020-02-17 15:15:30 +0200

    refreshmarathonc

--- t17/mx_hex_to_nbr.c
+++ t17/mx_hex_to_nbr.c
@@ -1,4 +1,15 @@
-static int get_dec_digit(char c);
+static int get_dec_digit(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    }
+    if (c >= 'A' && c <= 'F') {
+        return c - 'A' + 10;
+    }
+    if (c >= 'a' && c <= 'f') {
+        return c - 'a' + 10;
+    }
+    return 0;
+}
 
 unsigned long mx_hex_to_nbr(const char *hex) {
     unsigned long n = 0;
@@ -11,18 +22,5 @@ unsigned long mx_hex_to_nbr(const char *hex) {
         hex++;
     }
     return n;
-}
-
-static int get_dec_digit(char c) {
-    if (c >= '0' && c <= '9') {
-        return c - '0';
-    }
-    if (c >= 'A' && c <= 'F') {
-        return c - 'A' + 10;
-    }
-    if (c >= 'a' && c <= 'f') {
-        return c - 'a' + 10;
-    }
-    return 0;
 }
 
